3a4797a7b7a086f7b165c92c986316f0a2dd0a9d
Natalia Franquet <n.franquet.lm@gmail.com>
2019-05-09 13:23:01 +0200

    heroku test

---
+++ server/public/precache-manifest.0cc1ac5189152f9d2da8e04cf3817131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9459d80c32383f68188fb649aaaed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0d62746a60ed05d8b7",
+    "url": "/static/css/main.914c91e2.chunk.css"
+  },
+  {
+    "revision": "7e52c9737b7b9bb03a73",
+    "url": "/static/js/2.3a48b2d0.chunk.js"
+  },
+  {
+    "revision": "3b0d62746a60ed05d8b7",
+    "url": "/static/js/main.c334ad12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
