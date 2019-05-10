2df87bd7f2fc6922018071006dfcd584597af340
laura <lauraabad83@gmail.com>
2019-05-10 20:40:01 +0200

    display genres

--- front/src/components/Events.js
+++ front/src/components/Events.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ArtService from './art-service';
+
 
 class Events extends React.Component {
+constructor(props){
+  super(props);
+  this.state= {
+    allGenres: [],
 
-  handleLogout = () => {
+  }
+this.ArtService= new ArtService()
+
+}
+
+componentDidMount(){
+this.ArtService.allGenres()
+.then(artworks => {
+  // console.log(artworks);
+  
+  this.setState({
+    ...this.state,
+    allGenres: artworks
+  })
+  // console.log(this.state)
+})
+
+}
+  
+
+handleLogout = () => {
     this.props.logout()
   }
 
   render() {
+    //  console.log(this.state.allGenres)
     return (
-      <div>
+      
+      <React.Fragment>
         <h1>LANDSCAPES from Events</h1>
         <Link to='/'>
           <button onClick={() => this.handleLogout()}>Logout</button>
         </Link>
-      </div>
+        {
+          this.state.allGenres.map((pict, idx)=>{
+            return (
+              <h2>{pict.genre}</h2>
+            )
+          })
+        }
+      </React.Fragment>
+
     )
   }
 }
