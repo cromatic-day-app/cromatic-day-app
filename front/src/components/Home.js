9e1c2534c66d8edb7865773dc323f09d17ac42be
lau <46005400+labad83@users.noreply.github.com>
2019-05-10 20:41:22 +0200

    Front api (#5)
    * added front-API
    
    * display genres

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -5,31 +5,13 @@ class Home extends React.Component {
     super(props);
     this.state = {
       loggedInUser: null,
-      // redirect: false,
     };
   }
 
-  // componentDidUpdate(){
-  //   if(!this.props.userInSession){
-  //     this.setState({
-  //       ...this.state,
-  //       redirect: true,
-  //     })
-  //   }
-  // }
-
-  // componentWillReceiveProps(nextProps) {
-  //   this.setState({ ...this.state, loggedInUser: nextProps["userInSession"] });
-  // }
-
-
   render() {
-    //if (!this.props.userInSession) return <Redirect to={"/login"}/>
     return (
       <React.Fragment>
-        {/* {
-          this.state.redirect ? <Redirect to="/login"/> : null
-        } */}
+      
         <h1>Home page</h1>
         
       </React.Fragment>
