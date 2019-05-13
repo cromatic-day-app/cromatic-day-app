218f7e5ae74ad1d62f6912fdcca5e930e5c1244a
lau <46005400+labad83@users.noreply.github.com>
2019-05-13 16:22:48 +0200

    ui collections (#10)

--- front/src/components/Partials/AllArtworks.js
+++ front/src/components/Partials/AllArtworks.js
@@ -1,53 +1,73 @@
-import React from 'react';
-import ArtService from '../art-service';
-
+import React from "react";
+import ArtService from "../art-service";
+import "bulma/css/bulma.css";
+import "./AllArtworks.css";
 class AllArtworks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       allArtworks: []
-    }
+    };
     this.ArtService = new ArtService();
   }
 
   componentWillReceiveProps(nextProps) {
-    this.ArtService.allArtworks(nextProps.selectedGenre)
-      .then(artworks => {
-        console.log(artworks);
-        this.setState({
-          ...this.state,
-          allArtworks: artworks
-        })
-      })
+    this.ArtService.allArtworks(nextProps.selectedGenre).then(artworks => {
+      console.log(artworks);
+      this.setState({
+        ...this.state,
+        allArtworks: artworks
+      });
+    });
   }
 
   componentDidMount() {
-    this.ArtService.allArtworks(this.props.selectedGenre)
-      .then(artworks => {
-        this.setState({
-          ...this.state,
-          allArtworks: artworks
-        })
-      })
-
+    this.ArtService.allArtworks(this.props.selectedGenre).then(artworks => {
+      this.setState({
+        ...this.state,
+        allArtworks: artworks
+      });
+    });
   }
 
   render() {
     return (
-      <div>
-        {
-          this.state.allArtworks.map((artwork, idx) => {
-            return (
-              <ul key={idx}>
-                <li>{artwork.title}</li>
-                <li><img src={artwork.primaryImageSmall} alt=""/></li>
-              </ul>
-            )
-          })
-        }
-        <h1>Soy el componente artWorks {this.props.selectedGenre}</h1>
+      <div id="cuadros" className="columns is-multiline is-8">
+        {this.state.allArtworks.map((artwork, idx) => {
+          return (
+            <div class="column is-one-third">
+              <div class="card" key={idx}>
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img
+                      src={artwork.primaryImageSmall}
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                     
+                    <div class="media-content">
+                      <p class="title is-4">{artwork.title}</p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. 
+                    {/* <a>@bulmaio</a>.<a href="#">#css</a> <a href="#">#responsive</a> */}
+                    <br />
+                    {/* <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time> */}
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
