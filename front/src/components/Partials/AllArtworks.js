9376de0883e74f7657d1d6ad8c7b42380807b021
Natalia Franquet <47147545+NataliaFranquet@users.noreply.github.com>
2019-05-14 09:33:33 +0200

    modal displayed (#14)

--- front/src/components/Partials/AllArtworks.js
+++ front/src/components/Partials/AllArtworks.js
@@ -2,24 +2,26 @@ import React from "react";
 import ArtService from "../art-service";
 import "bulma/css/bulma.css";
 import "./AllArtworks.css";
-import { Link } from 'react-router-dom';
 import ModalCard from './ModalCard';
 
 class AllArtworks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allArtworks: [],
-      isOpen: false
+      allArtworks: []
     };
     this.ArtService = new ArtService();
   }
 
-  handleShowDialog = () => {
-    this.setState({
-      isOpen: true
-    });
-    console.log('cliked');
+  showModal = (modalId) => {
+    console.log(modalId);
+    const modal = document.getElementById(modalId);
+    modal.className = "modal is-active";
+  }
+
+  closeModal = (modalId) => {
+    const modal = document.getElementById(modalId);
+    modal.className = "modal";
   }
 
   componentWillReceiveProps(nextProps) {
@@ -45,38 +47,40 @@ class AllArtworks extends React.Component {
   render() {
     return (
       <div id="cuadros" className="columns is-multiline is-8">
-        {this.state.allArtworks.map((artwork, idx) => {
-          return (
-            <div class="column is-one-third">
-              <div class="card" key={idx}>
-                <div class="card-image">
-                  <figure class="image is-4by3">
-                    <img
-                      src={artwork.primaryImageSmall}
-                      alt="Placeholder image"
-                    />
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                     
-                    <div class="media-content">
-                      <p class="title is-4">{artwork.title}</p>
+        {
+          this.state.allArtworks.map((artwork, idx) => {
+            return (
+              <div className="column is-one-third">
+                <div className="card" key={idx}>
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img
+                        src={artwork.primaryImageSmall}
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <div className="media">
+                      <div className="media-content">
+                        <p className="title is-4">{artwork.title}</p>
+                      </div>
+                    </div>
+                    <div className="content">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Phasellus nec iaculis mauris.
+                    {/* <a>@bulmaio</a>.<a href="#">#css</a> <a href="#">#responsive</a> */}
+                      <br />
+                      {/* <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time> */}
                     </div>
                   </div>
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris. 
-                    {/* <a>@bulmaio</a>.<a href="#">#css</a> <a href="#">#responsive</a> */}
-                    <br />
-                    {/* <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time> */}
-                  </div>
+                  <button onClick={() => this.showModal("m" + idx)}>Details</button>
                 </div>
+                <ModalCard artwork={artwork} idx={idx}></ModalCard>
               </div>
-
-            </div>
-          );
-        })}
+            );
+          })
+        }
       </div>
     );
   }
--- front/src/components/Partials/ModalCard.js
+++ front/src/components/Partials/ModalCard.js
@@ -1,30 +1,43 @@
 import React from 'react';
 
 class ModalCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  showModal = (modalId) => {
+    console.log(modalId);
+    const modal = document.getElementById(modalId);
+    modal.className = "modal is-active";
+  }
+
+  closeModal = (modalId) => {
+    const modal = document.getElementById(modalId);
+    modal.className = "modal";
+  }
+
   render() {
+    let artwork = this.props.artwork;
+    let idx = this.props.idx;
     return (
-      <div>
-        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">
-          Launch demo modal
-        </button>
-        <div className="modal fade" data-show="true" id="exampleModalScrollable" tabIndex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-          <div className="modal-dialog modal-dialog-scrollable" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                ...
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </div>
+      <div id={"m" + idx} className="modal">
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{artwork.title}</p>
+            <button className="delete" aria-label="close" onClick={() => this.closeModal("m" + idx)}></button>
+          </header>
+          <section className="modal-card-body">
+            <img
+              src={artwork.primaryImageSmall}
+              alt="Placeholder image"
+            />
+            <p>{artwork.date}</p>
+            <p>{artwork.price}€</p>
+          </section>
+          <footer className="modal-card-foot">
+            <button onClick={() => this.closeModal("m" + idx)} className="button">Cancel</button>
+          </footer>
         </div>
       </div>
     )
