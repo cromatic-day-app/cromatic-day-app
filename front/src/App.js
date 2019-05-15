7a2459cebfbeda75537a2391823ec704f5f987fb
laura <lauraabad83@gmail.com>
2019-05-15 20:32:00 +0200

    add ui about us

--- front/src/App.js
+++ front/src/App.js
@@ -11,121 +11,179 @@ import Profile from './components/Global/Profile';
 import AboutUs from './components/Global/AboutUs';
 import ModalCard from './components/Partials/ModalCard';
 import MainNav from './components/Partials/MainNav';
+import Voucher from './components/Global/Voucher';
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loggedInUser: null,
-      hide: false,
-      qty: 0
-    };
-    this.service = new AuthService();
-  }
-
-  addItem = () => {
-    this.setState({
-      ...this.state,
-      qty: this.state.qty + 1
-    });
-    // this.props.handleTotal(this.props.price);
-  }
-
-  toggleHeader = () => {
-    const { hide } = this.state
-    this.setState({
-      ...this.state,
-      hide: !hide
-    })
-  }
-
-  fetchUser() {
-    if (this.state.loggedInUser === null) {
-      this.service.loggedin()
-        .then(response => {
-          this.setState({
-            loggedInUser: response
-          })
-        })
-        .catch(err => {
-          this.setState({
-            loggedInUser: false
-          })
-        })
-    }
-  }
-
-  getUser = (userObj) => {
-    this.setState({
-      loggedInUser: userObj
-    })
-  }
-
-  logout = () => {
-    this.service.logout()
-      .then(() => {
-        this.setState({
-          loggedInUser: null
-        });
-      })
-  }
-
-  render() {
-    this.fetchUser();
-    return (
-      <React.Fragment>
-        {
-          !this.state.hide
-            ? <MainNav user={this.state.loggedInUser} qty={this.state.qty}></MainNav>
-            : <div>
-              <div className='topHeader'>
-                <div >
-                  <img src="../img/logo.png" alt="img" />
-                </div>
-              </div>
-              <div className='loggedInIcons'>
-                <Link className="logout-link" to='/' onClick={() => this.logout()}>Logout</Link>
-                <i className="fas fa-shopping-cart cart" />
-                {
-                  (this.state.qty) > 0 ? <span className="qty">{this.state.qty}</span> : null
-                }
-              </div>
-            </div>
+        constructor(props) {
+            super(props)
+            this.state = {
+                loggedInUser: null,
+                hide: false,
+                qty: 0
+            };
+            this.service = new AuthService();
         }
-        <Switch>
-          <Route exact path='/' component={Home} />
 
-          <Route exact path='/about-us' component={AboutUs} />
+        addItem = () => {
+            this.setState({
+                ...this.state,
+                qty: this.state.qty + 1
+            });
+            // this.props.handleTotal(this.props.price);
+        }
 
-          <Route exact path='/signup' render={() =>
-            this.state.loggedInUser ? <Redirect to={"/events"} /> :
-              <Signup />} />
+        toggleHeader = () => {
+            const { hide } = this.state
+            this.setState({
+                ...this.state,
+                hide: !hide
+            })
+        }
 
-          <Route exact path='/login' render={() =>
-            this.state.loggedInUser ? <Redirect to={"/events"} /> :
-              <Login getUser={this.getUser} />} />
+        fetchUser() {
+            if (this.state.loggedInUser === null) {
+                this.service.loggedin()
+                    .then(response => {
+                        this.setState({
+                            loggedInUser: response
+                        })
+                    })
+                    .catch(err => {
+                        this.setState({
+                            loggedInUser: false
+                        })
+                    })
+            }
+        }
 
-          <Route exact path='/events' render={() =>
-            this.state.loggedInUser ? <Events user={this.state.loggedInUser} addItem={() => this.addItem()} /> :
-              <Redirect to={'/login'} />} />
+        getUser = (userObj) => {
+            this.setState({
+                loggedInUser: userObj
+            })
+        }
 
-          <Route exact path='/events/:genre' render={() =>
-            this.state.loggedInUser ? <Events user={this.state.loggedInUser} addItem={() => this.addItem()} /> :
-              <Redirect to={'/login'} />} />
+        logout = () => {
+            this.service.logout()
+                .then(() => {
+                    this.setState({
+                        loggedInUser: null
+                    });
+                })
+        }
 
-          <Route exact path='/events/:genre/:artworkId' render={() =>
-            this.state.loggedInUser ? <ModalCard /> :
-              <Redirect to={'/login'} />} />
+        render() {
+                this.fetchUser();
+                return ( <
+                        React.Fragment > {!this.state.hide ?
+                            < MainNav user = { this.state.loggedInUser }
+                            qty = { this.state.qty } > < /MainNav> :
+                                < div >
+                                <
+                                div className = 'topHeader' >
+                                <
+                                div >
+                                <
+                                img src = "../img/logo.png"
+                            alt = "img" / >
+                            <
+                            /div> <
+                            /div> <
+                            div className = 'loggedInIcons' >
+                            <
+                            Link className = "logout-link"
+                            to = '/'
+                            onClick = {
+                                () => this.logout() } > Logout < /Link> <
+                            i className = "fas fa-shopping-cart cart" / > {
+                                (this.state.qty) > 0 ? < span className = "qty" > { this.state.qty } < /span> : null
+                            } <
+                            /div> <
+                            /div>
+                        } <
+                        Switch >
+                        <
+                        Route exact path = '/'
+                        component = { Home }
+                        />
 
-          <Route exact path='/profile' render={() =>
-            this.state.loggedInUser ? <Profile {...this.state.loggedInUser} getUser={this.getUser} toggleHeader={() => this.toggleHeader()} /> :
-              <Redirect to={'/login'} />} />
+                        <
+                        Route exact path = '/about-us'
+                        component = { AboutUs }
+                        />
 
-        </Switch>
-      </React.Fragment>
-    );
-  }
-}
+                        <
+                        Route exact path = '/signup'
+                        render = {
+                            () =>
+                            this.state.loggedInUser ? < Redirect to = { "/events" }
+                            /> : <
+                            Signup / >
+                        }
+                        />
 
-export default App;
+                        <
+                        Route exact path = '/login'
+                        render = {
+                            () =>
+                            this.state.loggedInUser ? < Redirect to = { "/events" }
+                            /> : <
+                            Login getUser = { this.getUser }
+                            />} / >
 
+                            <
+                            Route exact path = '/events'
+                            render = {
+                                () =>
+                                this.state.loggedInUser ? < Events user = { this.state.loggedInUser }
+                                addItem = {
+                                    () => this.addItem() }
+                                /> : <
+                                Redirect to = { '/login' }
+                                />} / >
+
+                                <
+                                Route exact path = '/events/:genre'
+                                render = {
+                                    () =>
+                                    this.state.loggedInUser ? < Events user = { this.state.loggedInUser }
+                                    addItem = {
+                                        () => this.addItem() }
+                                    /> : <
+                                    Redirect to = { '/login' }
+                                    />} / >
+
+                                    <
+                                    Route exact path = '/events/:genre/:artworkId'
+                                    render = {
+                                        () =>
+                                        this.state.loggedInUser ? < ModalCard / > :
+                                            <
+                                            Redirect to = { '/login' }
+                                        />} / >
+
+                                        <
+                                        Route exact path = '/profile'
+                                        render = {
+                                            () =>
+                                            this.state.loggedInUser ? < Profile {...this.state.loggedInUser }
+                                            getUser = { this.getUser }
+                                            toggleHeader = {
+                                                () => this.toggleHeader() }
+                                            /> : <
+                                            Redirect to = { '/login' }
+                                            />} / >
+
+                                            <
+                                            Route exact path = '/voucher'
+                                            component = { Voucher }
+                                            />    
+
+                                            <
+                                            /Switch> <
+                                            /React.Fragment>
+                                        );
+                                    }
+                                }
+
+                                export default App;
